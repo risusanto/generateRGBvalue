79622d34aff74fd5a439aae183c26480686870a9
Ari Susanto <Ari Susanto>
2017-01-18 22:03:53 +0700

    initial commit

---
+++ citra/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace citra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.image = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // image
+            // 
+            this.image.Image = ((System.Drawing.Image)(resources.GetObject("image.Image")));
+            this.image.Location = new System.Drawing.Point(15, 18);
+            this.image.Name = "image";
+            this.image.Size = new System.Drawing.Size(186, 146);
+            this.image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.image.TabIndex = 0;
+            this.image.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(221, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "get RGB";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(204, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Pilih Gambar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(218, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 240);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.image);
+            this.Name = "Form1";
+            this.Text = "RGB Value";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox image;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ citra/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace citra
+{
+    public partial class Form1 : Form
+    {
+        bool gambar = false;
+        string rgbValue;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rgbValue = "";
+            if (!gambar)
+            {
+                label4.Text = "Silahkan pilih gambar";
+            }
+            else
+            {
+                try
+                {
+                    getRGB();
+                }
+                catch
+                {
+                    label4.Text = "salah";
+                }
+            }
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Cari Gambar";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bm = new Bitmap(dlg.FileName);
+                image.Image = bm;
+                gambar = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Text = "";
+        }
+
+        void getRGB()
+        {
+            Bitmap a = new Bitmap(image.Image);
+            if (a.Height > 30 || a.Width > 30)
+                label4.Text = "Resulusi gambar terlalu besar! maksimal 30x30 pixel";
+            else
+            {
+                for (int i = 0; i < a.Width; i++)
+                {
+                    for (int j = 0; j < a.Height; j++)
+                    {
+                        Color clr = a.GetPixel(i, j);
+                        rgbValue = rgbValue + string.Format("f({0},{1})= RGB({2},{3},{4})", i, j, clr.R, clr.G, clr.B) + "\n";
+
+                    }
+                    rgbValue = rgbValue + "\n";
+                }
+                SaveFileDialog save1 = new SaveFileDialog();
+                save1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                save1.FilterIndex = 2;
+                save1.RestoreDirectory = true;
+                if (save1.ShowDialog() == DialogResult.OK)
+                {
+                    System.IO.File.WriteAllText(save1.FileName, rgbValue);
+                    label4.Text = "File tersimpan sebagai:\n" + save1.FileName;
+                }
+            }
+        }
+    }
+}
